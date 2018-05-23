0749e9a6662a813662baa526988aee11d8794a94
Ale Chaito <alechaito@gmail.com>
2018-05-23 04:01:30 -0300

    create server and threading client conections

--- main.py
+++ main.py
@@ -1,49 +1,57 @@
 import socket
-
-
-
-HOST = '198.50.194.112'     # Endereco IP do Servidor
-PORT = 7171           # Porta que o Servidor esta
-MSG = "TEST"
+import time
+import traceback
+from _thread import *
+import threading
+import sys
 
 def main():
     tcp = TCP()
-    
-    tcp.client()
     tcp.server()
 
 
 class TCP():
-
     def __init__(self):
-        self.HOST = '198.50.194.112'  
-        self.PORT = 7171 
-        self.MSG = "TEST"
-
-    def client(self):
-        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        dest = (self.HOST, self.PORT)
-        tcp.connect(dest)
-        print('Para sair use CTRL+X')
-        tcp.send (self.MSG.encode())
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.HOST = '127.0.0.1'  
+        self.PORT = 5000
+        self.BUFFER = 1024
+        self.THREADS = []
 
     def server(self):
-        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         orig = (self.HOST, self.PORT)
-        tcp.bind(orig)
-        tcp.listen(1)
-        while True:
-            con, cliente = tcp.accept()
-            print('Concetado por', cliente)
-            while True:
-                msg = con.recv(1024)
-                if not msg: break
-                print(cliente, msg)
-            print('Finalizando conexao do cliente', cliente)
-            con.close()
+        try:
+            self.socket.bind((self.HOST, self.PORT))
+        except:
+            print("Bind failed. Error : " + str(sys.exc_info()))
+            sys.exit()
 
+        self.socket.listen(5)
+        while(True):
+            con, cliente = self.socket.accept()
+            client = Client(self.HOST, self.PORT, con) 
+            client.start()
 
+    
+class Client(threading.Thread):
+    def __init__(self, ip, port, con): 
+        threading.Thread.__init__(self)
+        self.ip = ip 
+        self.port = port
+        self.con = con
+        self.WELCOME_MSG = "Welcome to the file server.."
+        print("[+] New server socket thread started for "+ip+":"+str(port))
+    
+    def run(self):    
+        print("[+] Connection from : "+self.ip+":"+str(self.port))
+        
+        self.con.send(self.WELCOME_MSG.encode())
 
+        data = "init"
+        while len(data):
+            data = self.con.recv(1024)
+            print("Client sent :"+ str(data) )
 
 
 
